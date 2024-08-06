f1962f717a6786818791b092119b66461a18cdd6
gamja3 <dwkimdwkim1005@gmail.com>
2024-08-07 01:44:30 +0900

    숫자들이 랜덤하게 섞인 배열, 정렬된 배열, 중복된 배열 정렬 테스트

--- src/sorting/advanced/PerformanceTest.java
+++ src/sorting/advanced/PerformanceTest.java
@@ -1,59 +1,46 @@
 package sorting.advanced;
 
+import sorting.GenerateArr;
+
 import java.util.*;
 
 public class PerformanceTest {
     public static void main(String[] args) {
-        AdvancedSort[] arr = {new MergeSort(),new SwitchingMergeSort(), new MyQuickSort()};
+        AdvancedSort[] arr = {new MergeSort(),new SwitchingMergeSort(), new MyQuickSort(), new QuickSort()};
 
-        int[] nums = generateArr(1_000_000);
-
-        test(arr,nums);
+        test(arr, 1000_000);
     }
 
-    private static void test(AdvancedSort[] arr, int[] nums){
+    private static void test(AdvancedSort[] arr, int size){
         for (AdvancedSort advancedSort : arr) {
-            int[] copiedNums = Arrays.copyOf(nums,nums.length);//배열 복사
+            int[] randomArr = GenerateArr.generateRandomArr(size);
+            int[] sortedArr = GenerateArr.generateSortedArr(size);
+            int[] duplicatedArr = GenerateArr.generateDuplicatedArr(size);
 
             System.out.println("============================");
             System.out.println(advancedSort.getClass());
+
             long start = System.currentTimeMillis();
-            advancedSort.sort(copiedNums,0,nums.length-1);
+            advancedSort.sort(randomArr,0,size-1);
             long end = System.currentTimeMillis();
             System.out.println("소요시간: "+(end-start)+"ms");
+
+            /*start = System.currentTimeMillis();
+            advancedSort.sort(sortedArr,0,size-1);
+            end = System.currentTimeMillis();
+            System.out.println("소요시간: "+(end-start)+"ms");*/
+
+            /*start = System.currentTimeMillis();
+            advancedSort.sort(duplicatedArr,0,size-1);
+            end = System.currentTimeMillis();
+            System.out.println("소요시간: "+(end-start)+"ms");*/
         }
 
-        System.out.println("============================");
+        /*System.out.println("============================");
         System.out.println("자바 제공 sort()");
         long start = System.currentTimeMillis();
         Arrays.sort(nums);
         long end = System.currentTimeMillis();
-        System.out.println("소요시간: "+(end-start)+"ms");
-    }
-
-    public static int[] generateArr(int size){
-        //중복 없이 랜덤한 순서의 배열 생성
-        Random random = new Random();
-        HashSet<Integer> set = new HashSet<>();
-
-        while (set.size() < size){
-            set.add(random.nextInt(size)+1);
-        }
-
-        int[] nums = new int[size];
-
-        ArrayList<Integer> list = new ArrayList<>(set);
-        Collections.shuffle(list);
-
-        for (int i = 0; i < size; i++) {
-            nums[i] = list.get(i);
-        }
-
-        /*int[] nums = new int[size];
-        for (int i = 0; i < size; i++) {
-            nums[i] = 100;
-        }*/
-
-        return nums;
+        System.out.println("소요시간: "+(end-start)+"ms");*/
     }
 }
