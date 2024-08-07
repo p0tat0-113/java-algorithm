c602d35b2cd7e45f59e3382c28d6b5af91b3a319
gamja3 <dwkimdwkim1005@gmail.com>
2024-08-08 01:18:34 +0900

    성능 테스트 코드 수정

--- src/sorting/advanced/PerformanceTest.java
+++ src/sorting/advanced/PerformanceTest.java
@@ -6,40 +6,37 @@ import java.util.*;
 
 public class PerformanceTest {
     public static void main(String[] args) {
-        AdvancedSort[] arr = {new MergeSort(),new SwitchingMergeSort(), /*new MyQuickSort(),*//* new QuickSort()*/ new QuickSortUpgrade(), new MyQuickSortUpgrade()};
+        AdvancedSort[] algorithm = {new MergeSort(),new SwitchingMergeSort(), new QuickSortUpgrade(), new MyQuickSortUpgrade(), new HeapSort()};
 
-        test(arr, 100_000);
+        test(algorithm, 100_000);
     }
 
-    private static void test(AdvancedSort[] arr, int size){
-        for (AdvancedSort advancedSort : arr) {
-            int[] randomArr = GenerateArr.generateRandomArr(size);
-            int[] sortedArr = GenerateArr.generateSortedArr(size);
-            int[] reversedSortedArr = GenerateArr.generateReversedSortedArr(size);
-            int[] duplicatedArr = GenerateArr.generateDuplicatedArr(size);
+    private static void test(AdvancedSort[] algorithm, int size){
+        int[] randomArr = GenerateArr.generateRandomArr(size);
+        int[] sortedArr = GenerateArr.generateSortedArr(size);
+        int[] reversedSortedArr = GenerateArr.generateReversedSortedArr(size);
+        int[] duplicatedArr = GenerateArr.generateDuplicatedArr(size);
+
+        for (AdvancedSort advancedSort : algorithm) {
+            int[] a1 = Arrays.copyOf(randomArr,size);
+            int[] a2 = Arrays.copyOf(sortedArr,size);
+            int[] a3 = Arrays.copyOf(reversedSortedArr,size);
+            int[] a4 = Arrays.copyOf(duplicatedArr,size);
+
+            int[][] arrs = {a1,a2,a3,a4};
 
             System.out.println("============================");
             System.out.println(advancedSort.getClass());
 
-            long start = System.currentTimeMillis();
-            advancedSort.sort(randomArr,0,size-1);
-            long end = System.currentTimeMillis();
-            System.out.println("소요시간: "+(end-start)+"ms");
+            for (int[] arr : arrs) {
+                long start = System.currentTimeMillis();
 
-            start = System.currentTimeMillis();
-            advancedSort.sort(sortedArr,0,size-1);
-            end = System.currentTimeMillis();
-            System.out.println("소요시간: "+(end-start)+"ms");
+                advancedSort.sort(randomArr,0,size-1);
 
-            start = System.currentTimeMillis();
-            advancedSort.sort(reversedSortedArr,0,size-1);
-            end = System.currentTimeMillis();
-            System.out.println("소요시간: "+(end-start)+"ms");
-
-            start = System.currentTimeMillis();
-            advancedSort.sort(duplicatedArr,0,size-1);
-            end = System.currentTimeMillis();
-            System.out.println("소요시간: "+(end-start)+"ms");
+                long end = System.currentTimeMillis();
+                System.out.print("소요시간: "+(end-start)+"ms ");
+            }
+            System.out.println();
         }
 
         /*System.out.println("============================");
