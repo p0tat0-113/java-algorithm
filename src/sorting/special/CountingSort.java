c5ffef855239273d8ab6c65df1788a8621e4bc57
gamja3 <dwkimdwkim1005@gmail.com>
2024-08-10 20:35:10 +0900

    주석 수정

--- src/sorting/special/CountingSort.java
+++ src/sorting/special/CountingSort.java
@@ -1,10 +1,11 @@
 package sorting.special;
 
 /*계수 정렬, 원소들이 특정 범위를 넘지 않는 특수한 경우에 사용할 수 있음. 얘도 안정정렬이고(구현하기에 따라서 다름, 아주 약간의 차이), Θ(n)의 수행시간을 가진다.
-* 별도의 카운팅 배열을 운용해서 각 원소가 각각 몇번 나왔는지 세고, 각 인덱스별로 구간합을 구함. 각 구간합은 그 숫자보다 작거나 같은 모든 숫자들의 갯수의 합을 가리킨다.
+* 별도의 카운팅 배열을 운용해서 각 원소가 각각 몇번 나왔는지 세고, 각 인덱스별로 구간합을 구함. 각 누적합은 그 숫자보다 작거나 같은 모든 숫자들의 갯수의 합을 가리킨다.
 * 이 구간합을 이용해서 원소들을 알맞은 자리에 집어넣음.
 *
 * 나는 음수가 포함되어도 정렬할 수 있게 구현함.
+* 값의 범위가 작을수록 정렬속도가 빠르다. 숫자는 몇개 없는데, 값의 범위만 크면 카운터 배열만 겁나게 커져야 하기 때문에 비효율적이다.
 */
 
 
@@ -29,7 +30,7 @@ public class CountingSort {
             countArr[i+cali] = countArr[i+cali]+1;
         }
 
-        //구간합을 구함.
+        //누적합을 구함. 각 인덱스에는 나보다 작거나 같은 숫자들이 몇개가 있는지가 들어감.
         for (int i = 1; i < countArr.length; i++) {
             countArr[i] = countArr[i] + countArr[i-1];
         }
@@ -37,8 +38,8 @@ public class CountingSort {
         //구간합에 맞춰서 각 숫자들을 집어넣음. 이때 안정적으로 정렬되게끔 해야 함.
         int[] sortedArr = new int[arr.length];
         for (int i = arr.length-1; i >= 0; i--) {
-            sortedArr[ countArr[arr[i]+cali] - 1 ] = arr[i]; //countArr[arr[i]+cali]해당 숫자의 countArr 구간합을 구한다. cali는 음수 때문에 더해주는 보정값이다.
-            countArr[arr[i]+cali] = countArr[arr[i]+cali]-1; //다음에 똑같은 숫자가 올 것에 대비해서 구간합을 하나 줄임.
+            sortedArr[ countArr[arr[i]+cali] - 1 ] = arr[i]; //countArr[arr[i]+cali]해당 숫자의 countArr 누적합을 구한다. cali는 음수 때문에 더해주는 보정값이다.
+            countArr[arr[i]+cali] = countArr[arr[i]+cali]-1; //다음에 똑같은 숫자가 올 것에 대비해서 누적합을 하나 줄임.
         }
         return sortedArr;
     }
