4ddaa9e554bc8d1a5de14b8a9baf1e214f0948d2
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-01 00:37:15 +0900

    람다 표현식을 사용할 때 뒤에서 실제로 어떤 일이 일어나는지 탐구. 함수형 인터페이스 - Predicate, Consumer, Function

--- src/java_study/functional_programming/FunctionalInterface.java
+++ src/java_study/functional_programming/FunctionalInterface.java
@@ -2,6 +2,7 @@ package java_study.functional_programming;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class FunctionalInterface {
@@ -11,6 +12,7 @@ public class FunctionalInterface {
         //리스트에서 홀수만 출력
         List.of(23,13,52,62).stream()
                 .filter(e -> e%2==0)
+                .map(e -> e*e)
                 .forEach(e -> System.out.println(e));
         //화면 뒤에서 무슨 일이 일어나고 있을까?
         //어떻게 'e -> e%2==1'이 코드 조각이 filter()메서드에 전달되고 있을까?
@@ -23,13 +25,17 @@ public class FunctionalInterface {
 
         List.of(23,13,52,62).stream()
                 .filter(new EvenNumverPredicate())//Predicate인터페이스를 구현한 클래스의 인스턴스를 집어넣음.
+                .map(new NumberSquareMapper())
                 .forEach(new PrintConsumer());
 
-        //.filter(e -> e%2==0) 이 코드를 쓸 때 위와 같은 현상이 똑같이 일어난 것이다. 인터페이스 구현과 인스턴스 생성을 동시에 하는 익명클래스에서 더 발전한 것이 람다라고 배운적이 있지.
-        //Predicate인터페이스는 테스트를 통과했는지 여부를 반환해주는 역할을 한다.
+        //filter(e -> e%2==0) 이 코드를 쓸 때 위와 같은 현상이 똑같이 일어난 것이다. 인터페이스 구현과 인스턴스 생성을 동시에 하는 익명클래스에서 더 발전한 것이 람다라고 배운적이 있지.
+        //filter()가 작동되는데는 Predicate인터페이스가 존재하고, filter()안에 넣는 람다표현식은 Predicate인터페이스의 test메서드를 재정의한다는 것이다.
+
+        //나머지 forEach와 map이 작동되는 이면도 똑같다.
     }
 
     //Predicate인터페이스를 구현해본다. Predicate인터페이스에서 유일한 추상메서드인 test를 오버라이딩함.
+    //Predicate인터페이스는 테스트를 통과했는지 여부를 반환해주는 역할을 한다.
     private static class EvenNumverPredicate implements Predicate<Integer>{
         @Override
         public boolean test(Integer number) {
@@ -38,10 +44,20 @@ public class FunctionalInterface {
     }
 
     //이번에는 forEach()가 인수로 받는 Consumer인터페이스를 구현해본다. Consumer인터페이스에서 유일한 추상메서드인 accept를 오버라이딩함.
+    //Comsumer는 아무것도 반환하지 않음. 소비하기만 한다.
     private static class PrintConsumer implements Consumer<Integer>{
         @Override
         public void accept(Integer number) {
             System.out.println(number);
+        }
+    }
+
+    //map()이 인수로 받는 Function인터페이스를 구현해본다. Function인터페이스에서 유일한 추상메서드인 apply를 오버라이딩했음.
+    //Functioin은 입력값을 얻고 출력하는 역할을 한다.
+    private static class NumberSquareMapper implements Function<Integer,Integer> {//입력,출력 타입 인수
+        @Override
+        public Integer apply(Integer number) {
+            return number * number;
         }
     }
 }
