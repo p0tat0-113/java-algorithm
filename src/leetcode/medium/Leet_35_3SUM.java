a6230dd2f94b851b06f6338cbd97189cb6010b9d
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-08 02:05:55 +0900

    리트코드 35번 문제, 최적화

--- src/leetcode/medium/Leet_35_3SUM.java
+++ src/leetcode/medium/Leet_35_3SUM.java
@@ -3,6 +3,9 @@ package leetcode.medium;
 /*
 https://imgur.com/a/2JUGibl
 투포인터 방식으로 탐색하면 될 듯함. 만들긴 만들었는데 시간이 너무 오래걸림. 114ms로 29프로를 땄고, 메모리는 겨우 10프로를 땀.
+
+solution을 보고 힌트를 얻었음. 투포인터 탐색 범위를 줄여나가면 숫자 조합 중복문제도 해결하고, 탐색범위도 줄일 수 있다!
+개선 후 41ms로 훨씬 빨라졌다.
 */
 
 import java.util.*;
@@ -14,6 +17,59 @@ public class Leet_35_3SUM {
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> result = new HashSet<>();//이미 나온 숫자들의 조합을 담아두는 set
+        HashSet<Integer> logSet = new HashSet<>();//이미 체크한 num을 담아두는 set
+
+        Arrays.sort(nums);//투포인터를 쓰기 위해 우선 정렬한다.
+
+        int head;
+        int tail;
+        int num;
+        int targetNum;
+
+        for (int i = 0; i < nums.length; i++) {
+            head = i+1;//어차피 투포인터 탐색범위를 하나씩 줄여나갈 것이기 때문에 head는 i+1로 잡는다.
+            tail = nums.length-1;
+
+            if (head >= tail) {
+                break;
+            }
+
+            num = nums[i];
+            if (logSet.contains(num)) {//이미 체크한 숫자라면 다시 for문 처음으로 돌아감.
+                continue;
+            }
+            targetNum = 0 - num;
+
+            while (true) {
+
+                //head가 tail과 같거나 더 커지면 탐색 중단.
+                if (head >= tail) {
+                    break;
+                }
+
+                if (nums[head] + nums[tail] == targetNum) {
+                    ArrayList<Integer> list = new ArrayList<>(List.of(num, nums[head], nums[tail]));
+                    result.add(list);
+
+                    head++;
+                    tail--;
+                }
+
+                //투포인터 탐색
+                if (nums[head] + nums[tail] < targetNum) {
+                    head++;
+                } else if (nums[head] + nums[tail] > targetNum) {
+                    tail--;
+                }
+            }
+            logSet.add(num);
+        }
+
+        return new ArrayList<>(result);
+    }
+
+    /*public List<List<Integer>> threeSum(int[] nums) {
         HashSet<List<Integer>> result = new HashSet<>();//이미 나온 숫자들의 조합을 담아두는 set
         HashSet<Integer> logSet = new HashSet<>();//이미 체크한 num을 담아두는 set
 
@@ -66,5 +122,5 @@ public class Leet_35_3SUM {
         }
 
         return new ArrayList<>(result);
-    }
+    }*/
 }
