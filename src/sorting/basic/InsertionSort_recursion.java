bbae8225c156b831c5fb81035e1310123345433e
p0tat0-113 <101091642+p0tat0-113@users.noreply.github.com>
2024-09-10 12:52:02 +0900

    주석 수정

--- src/sorting/basic/InsertionSort_recursion.java
+++ src/sorting/basic/InsertionSort_recursion.java
@@ -20,13 +20,13 @@ public class InsertionSort_recursion {
         if (n > 1){//n == 1이면 그대로 종료. 배열의 크기가 1이므로 그대로 두는 것이 정렬된 상태이다.
             sort(arr,n-1);//크기가 하나 작은 문제를 호출, 이 호출이 리턴됐을 때는 arr[0...n-1]은 정렬된 상태이다.
 
-            int newItem = arr[n-1];
+            int newItem = arr[n-1];//비교될 수
             int i;
             for (i = n-1; i > 0; i--) {
-                if (newItem > arr[i-1]){
+                if (newItem > arr[i-1]){//들어갈 자리를 찾으면 탐색을 멈춘다
                     break;
                 }
-                arr[i] = arr[i-1];
+                arr[i] = arr[i-1];//숫자를 뒤로 한 칸씩 민다.
             }
             arr[i] = newItem;
         }
