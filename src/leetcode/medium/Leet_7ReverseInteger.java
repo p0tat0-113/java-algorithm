f608c69545009ec925af084db9c2f56af0dea6de
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-14 15:49:54 +0900

    리트코드 7번 문제 푸는 중... 이 문제는 문자열의 관점에서도 접근할 수 있고, 수학적인 관점에서도 접근할 수 있다.

---
+++ src/leetcode/medium/Leet_7ReverseInteger.java
@@ -0,0 +1,17 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class Leet_7ReverseInteger {
+    public static void main(String[] args) {
+        int num = 2147483647;
+        System.out.println(num++);
+        System.out.println(num);
+
+        String strNum = "2147483647";
+        System.out.println(Integer.parseInt(strNum));
+
+        System.out.println(String.valueOf(-10));
+        System.out.println(Arrays.toString(String.valueOf(-10).split("")));
+    }
+}
