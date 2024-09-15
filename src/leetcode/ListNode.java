b1fa4afdfa1cedddb67892e29b413beb61de56eb
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-15 15:36:22 +0900

    ListNode 클래스 생성

---
+++ src/leetcode/ListNode.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+//자주 쓰이는 ListNode를 아예 별개의 클래스로 분리함. 매번 복붙하지 말고, 임포트해서 쓰자.
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        ListNode node = this;
+        StringBuilder sb = new StringBuilder();
+        while (node != null) {
+            sb.append(node.val);
+            if (node.next != null) {
+                sb.append(" -> ");
+            }
+            node = node.next;
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ListNode(1, new ListNode(2, new ListNode(3, null))));
+    }
+}
