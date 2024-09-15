b9cf87b7859a52371c1644b438cc1f081a0bca1f
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-15 15:13:22 +0900

    리트코드 287번 문제 해결, 배열을 연결리스트의 관점에서 보아야 한다는 점, 그렇게 봤을 때 반드시 사이클이 존재하고, 사이클의 시작점이 바로 중복된 숫자라는 것, 중복된 숫자로 인해 사이클이 시작된다는 점은 파악했으나, 그 지점을 상수메모리만 사용해서 파악하는데 어려움을 겪었다. 결국 플로이드의 토끼와 거북이 알고리즘을 사용해서 해결함.

--- src/leetcode/medium/Leet_287FindTheDuplicateNumber.java
+++ src/leetcode/medium/Leet_287FindTheDuplicateNumber.java
@@ -6,11 +6,34 @@ import java.util.HashSet;
 public class Leet_287FindTheDuplicateNumber {
     public static void main(String[] args) {
         Leet_287FindTheDuplicateNumber leet = new Leet_287FindTheDuplicateNumber();
-        System.out.println(leet.findDuplicate(new int[]{1, 3, 4, 2, 2}));
+        System.out.println(leet.findDuplicate(new int[]{1,3,3,2,4}));
+    }
+
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
     }
 
     //이 코드로 사이클이 시작되는 수가 중복되는 수라는 가설을 검증함. 성공했다. 이제 set만 어떻게든 제거하면 됨.
-    public int findDuplicate(int[] nums) {
+    //리트코드에서 analyze 해본 결과 공간복잡도는 여전히 O(n)이다. 상수공간을 사용하면서도 사이클의 시작지점을 찾을 수 있는 방법이 필요하다.
+    /*public int findDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>(nums.length);
 
         int num = nums[0];//num은 실제 숫자, 이 숫자는 다음으로 오는 숫자의 인덱스다.
@@ -22,7 +45,7 @@ public class Leet_287FindTheDuplicateNumber {
             set.add(num);
             num = nums[num];
         }
-    }
+    }*/
 
 
     //아래의 두 풀이방법은, 문제의 조건에 맞지 않는 풀이방법이다. 그리고 제출했을 때 수행시간이 느린걸 보니까 O(logn)의 풀이 방법이 있는 것으로 보인다.
