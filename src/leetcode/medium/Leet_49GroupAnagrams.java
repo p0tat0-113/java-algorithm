5d152bbcd8e09ab2c75205014398254b4574f0f1
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-23 12:07:18 +0900

    리트코드 49번 문제, 문자열 정렬과 해시테이블을 사용하는 문제. 최적화 필요

---
+++ src/leetcode/medium/Leet_49GroupAnagrams.java
@@ -0,0 +1,47 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/*
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+이런 문제다. 나는 우선 각 단어를 char배열로 만들고 정렬한 후 다시 String으로 만들어서 map을 이용해 이걸 비교하는 식으로 문제를 풀었다.
+간단한 코드로 해결하긴 했는데, 실행시간이 10ms로 그닥 좋지는 못하다. 6ms까지 단축시켜야 함.
+
+정렬하지 않고 같은 알파벳들을 가진 단어를 찾아낼 수 있는 방법이 없을까?
+*/
+
+public class Leet_49GroupAnagrams {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap<>(strs.length);
+
+        char[] charArray;
+        String sortedStr;
+
+        for (String str : strs) {
+            charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            sortedStr = Arrays.toString(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
