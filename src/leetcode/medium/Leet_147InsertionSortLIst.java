b3aba62511927b43ed593ed4085579d21e4597bc
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-28 01:09:28 +0900

    리트코드 147번 문제, 학교 과제로 나와서 다시 풀어봤다. 그런데 수행시간을 좀 더 줄이기 위해서는 기존과 다른 새로운 접근방식이 필요해 보인다.

--- src/leetcode/medium/Leet_147InsertionSortLIst.java
+++ src/leetcode/medium/Leet_147InsertionSortLIst.java
@@ -19,41 +19,74 @@ public class Leet_147InsertionSortLIst {
         System.out.println(leet147InsertionSortLIst.insertionSortList(root));
     }
 
+    //과제로 받고 다시 짜본 코드, 근데 똑같은 아이디어를 기반으로 만들다 보니까 거의 비슷한 코드가 나올 수 밖에 없음.
     public ListNode insertionSortList(ListNode head) {
-        ListNode tempHeadNode = new ListNode(-10000, head);//임시로 머리가 되어줄 노드, 맨 앞에 삽입해야 하는 경우를 위해서 이렇게 했다.일부로 값을 -10000으로 해서 얘보다 작은 노드가 나올 수 없게 함.
-        ListNode currentNode = tempHeadNode;//node변수에 담겨있는 노드와 비교되는 노드를 가리키는 변수, 맨 앞에서부터 쭉 훑게 될 것이다. currentNode는 비교하는 노드보다 한 칸 앞의 노드를 가리킨다. 그래야 필요할 때 node를 그 앞에 삽입할 수 있기 때문이다.
-        ListNode tailOfSmallList = head;//부분 리스트의 맨 끝 노드를 가리키는 변수
+        ListNode tempHead = new ListNode();
+        tempHead.next = head;
 
-        ListNode node = tailOfSmallList.next;//비교하고 삽입될 노드, 부분 배열의 맨 끝에 노드에 바로 붙어있는 노드다.
+        ListNode sortedListTailNode = head;//정렬된 리스트의 맨 끝 노드
+        ListNode currentNode = tempHead;
 
-        while (node != null) {
-            //이 부분 하나 바꿔준걸로 리트코드 상에서 실행속도가 꽤 빨라짐.
-            while (currentNode != tailOfSmallList) {//개선사항 currentNode != node -> currentNode != tailOfSmallList currentNode가 tailOfSmallList를 가리키게 되면 currentNode.next는 node를 가리키게 되기 때문에 아무런 의미가 없음.
-                if (currentNode.next.val > node.val) {//node가 중간에 들어갈 자리를 찾았으면
-                    tailOfSmallList.next = node.next;//tailOfSmallList -> node -> node.next 연결을 tailOfSmallList -> node.next 이렇게 만든다. node를 깍두기로 만들어버림ㅠ
-                    //node.next = null; 바로 뒤에서 node.next = currentNode.next 연산을 하기 때문에 무의미하다.
+        head = head.next;//이제부터 head는 정렬된 부분배열에 새롭게 집어넣을 숫자를 가리킴.
+        while (head != null) {
+            while (currentNode != sortedListTailNode) {//연산을 할 때 다 currentNode.next로 하는데, currentNode == sortedListTailNode이면 currentNode.next == head가 됨.
+                if (head.val < currentNode.next.val) {//참이 되면 head를 이 사이에 삽입을 해야한다.
+                    sortedListTailNode.next = head.next;
 
-                    //node를 맞는 위치에 중간에 삽입함.
-                    node.next = currentNode.next;
-                    currentNode.next = node;
-
-                    break;
+                    head.next = currentNode.next;
+                    currentNode.next = head;
+                    break;//이 부분과
                 }
-
-                currentNode = currentNode.next;
+                currentNode = currentNode.next;//이 부분이 없어서 숫자 비교를 멈추지 않고 계속 하고 있었음.
             }
 
-            if (tailOfSmallList.next == node) {//노드가 그냥 제자리에 그대로 있는 경우 tailOfSmallList를 node로 업데이트 해준다. node가 중간에 삽입됐다면 tailOfSmallList를 그대로 유지하면 된다.
-                tailOfSmallList = node;
+            if (head != sortedListTailNode.next) {//새로운 숫자가 정렬된 리스트의 중간에 삽입된 경우
+                head = sortedListTailNode.next;
+            } else {//새로운 숫자가 정렬된 리스트 중에서 가장 커서 그냥 그대로 있는 경우
+                sortedListTailNode = head;
+                head = head.next;
             }
 
-            node = tailOfSmallList.next;//다음으로 비교하고 삽입될 노드 설정
-            currentNode = tempHeadNode;//currentNode 초기화
+            currentNode = tempHead;
         }
-
-        //처음에 return head라고 해서 4 -> 만 출력돼서 좀 헤맸음. 정렬 과정 중에 head가 가리키는 노드의 위치가 계속 바뀌기 때문에 tempHeadNode.next를 반환해야 한다.
-        return tempHeadNode.next;
+        return tempHead.next;
     }
+
+//    /*public ListNode insertionSortList(ListNode head) {
+//        ListNode tempHeadNode = new ListNode(-10000, head);//임시로 머리가 되어줄 노드, 맨 앞에 삽입해야 하는 경우를 위해서 이렇게 했다.일부로 값을 -10000으로 해서 얘보다 작은 노드가 나올 수 없게 함.
+//        ListNode currentNode = tempHeadNode;//node변수에 담겨있는 노드와 비교되는 노드를 가리키는 변수, 맨 앞에서부터 쭉 훑게 될 것이다. currentNode는 비교하는 노드보다 한 칸 앞의 노드를 가리킨다. 그래야 필요할 때 node를 그 앞에 삽입할 수 있기 때문이다.
+//        ListNode tailOfSmallList = head;//부분 리스트의 맨 끝 노드를 가리키는 변수
+//
+//        ListNode node = tailOfSmallList.next;//비교하고 삽입될 노드, 부분 배열의 맨 끝에 노드에 바로 붙어있는 노드다.
+//
+//        while (node != null) {
+//            //이 부분 하나 바꿔준걸로 리트코드 상에서 실행속도가 꽤 빨라짐.
+//            while (currentNode != tailOfSmallList) {//개선사항 currentNode != node -> currentNode != tailOfSmallList currentNode가 tailOfSmallList를 가리키게 되면 currentNode.next는 node를 가리키게 되기 때문에 아무런 의미가 없음.
+//                if (currentNode.next.val > node.val) {//node가 중간에 들어갈 자리를 찾았으면
+//                    tailOfSmallList.next = node.next;//tailOfSmallList -> node -> node.next 연결을 tailOfSmallList -> node.next 이렇게 만든다. node를 깍두기로 만들어버림ㅠ
+//                    //node.next = null; 바로 뒤에서 node.next = currentNode.next 연산을 하기 때문에 무의미하다.
+//
+//                    //node를 맞는 위치에 중간에 삽입함.
+//                    node.next = currentNode.next;
+//                    currentNode.next = node;
+//
+//                    break;
+//                }
+//
+//                currentNode = currentNode.next;
+//            }
+//
+//            if (tailOfSmallList.next == node) {//노드가 그냥 제자리에 그대로 있는 경우 tailOfSmallList를 node로 업데이트 해준다. node가 중간에 삽입됐다면 tailOfSmallList를 그대로 유지하면 된다.
+//                tailOfSmallList = node;
+//            }
+//
+//            node = tailOfSmallList.next;//다음으로 비교하고 삽입될 노드 설정
+//            currentNode = tempHeadNode;//currentNode 초기화
+//        }
+//
+//        //처음에 return head라고 해서 4 -> 만 출력돼서 좀 헤맸음. 정렬 과정 중에 head가 가리키는 노드의 위치가 계속 바뀌기 때문에 tempHeadNode.next를 반환해야 한다.
+//        return tempHeadNode.next;
+//    }*/
 
     private static class ListNode{
         int val;
