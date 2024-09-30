3a945093ba347985309e0e28a4f7d795f48a7262
gamja3 <dwkimdwkim1005@gmail.com>
2024-09-30 13:14:45 +0900

    학교 과제 제출용 정렬 알고리즘 성능 테스트 코드

---
+++ src/sorting/SchoolSortingPerformanceTest.java
@@ -0,0 +1,77 @@
+package sorting;
+
+import sorting.advanced.*;
+import sorting.basic.InsertionSort;
+import sorting.basic.Sort;
+
+import java.util.Arrays;
+
+public class SchoolSortingPerformanceTest {
+    public static void main(String[] args) {
+        AdvancedSort[] algorithms1 = {new MergeSort(),new SwitchingMergeSort(), new MyQuickSortUpgrade(), new HeapSort()};
+        testAdvanced(algorithms1, 10000);
+
+        Sort[] algorithms2 = {new InsertionSort()};
+        testBasic(algorithms2, 1000_000);
+    }
+
+    private static void testAdvanced(AdvancedSort[] algorithm, int size) {
+        int[] randomArr = GenerateArr.generateRandomArr(size);
+        int[] sortedArr = GenerateArr.generateSortedArr(size);
+        int[] duplicatedArr = GenerateArr.generateDuplicatedArr(size);
+
+        System.out.println("랜덤 배열, 정렬된 배열, 모든 수가 중복된 배열 순서대로 정렬한다.");
+
+        for (AdvancedSort advancedSort : algorithm) {
+            //각 정렬 알고리즘으로 랜덤 배열, 정렬된 배열, 모든 요소가 중복된 배열을 정렬한다.
+            int[] a1 = Arrays.copyOf(randomArr, size);
+            int[] a2 = Arrays.copyOf(sortedArr, size);
+            int[] a3 = Arrays.copyOf(duplicatedArr, size);
+
+            int[][] arrs = {a1, a2, a3};
+
+            System.out.println("============================");
+            System.out.println(advancedSort.getClass().getTypeName());//정렬 방식 이름 출력
+
+            for (int[] arr : arrs) {
+                long start = System.nanoTime();
+
+                advancedSort.sort(arr, 0, size - 1);
+
+                long end = System.nanoTime();
+                System.out.print("소요시간: " + (end - start)/1_000_000_000.0 + "s ");//나노초를 초로 변환해서 출력
+            }
+            System.out.println();
+        }
+    }
+
+    private static void testBasic(Sort[] algorithm, int size) {
+        int[] randomArr = GenerateArr.generateRandomArr(size);
+        int[] sortedArr = GenerateArr.generateSortedArr(size);
+        int[] duplicatedArr = GenerateArr.generateDuplicatedArr(size);
+
+        System.out.println("랜덤 배열, 정렬된 배열, 모든 수가 중복된 배열 순서대로 정렬한다.");
+
+        for (Sort sort : algorithm) {
+            //각 정렬 알고리즘으로 랜덤 배열, 정렬된 배열, 모든 요소가 중복된 배열을 정렬한다.
+            int[] a1 = Arrays.copyOf(randomArr, size);
+            int[] a2 = Arrays.copyOf(sortedArr, size);
+            int[] a3 = Arrays.copyOf(duplicatedArr, size);
+
+            int[][] arrs = {a1, a2, a3};
+
+            System.out.println("============================");
+            System.out.println(sort.getClass().getTypeName());//정렬 방식 이름 출력
+
+            for (int[] arr : arrs) {
+                long start = System.nanoTime();
+
+                sort.sort(arr);
+
+                long end = System.nanoTime();
+                System.out.print("소요시간: " + (end - start)/1_000_000_000.0 + "s ");//나노초를 초로 변환해서 출력
+            }
+            System.out.println();
+        }
+    }
+}
