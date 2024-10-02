2445ba1732ca4167ee407a73f37e01bcf7487e38
gamja3 <dwkimdwkim1005@gmail.com>
2024-10-03 00:02:59 +0900

    리트코드 1497번 문제, 길이가 짝수인 배열에서 합쳐서 k로 나누어질 수 있는 쌍을 길이/2개 만들 수 있는지 검사해야 하는 문제. 하나하나 쌍을 만드는 식으로는 시간이 너무 오래 걸려서 해결할 수 없다.

---
+++ src/leetcode/medium/Leet_1497CheckIfArrayPairsAreDivisibleByK.java
@@ -0,0 +1,95 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class Leet_1497CheckIfArrayPairsAreDivisibleByK {
+    public static void main(String[] args) {
+        //List는 이렇게 set을 이용해서 중복을 제거할 수 있다. List에서 이미 hashCode를 오버라이딩 해놓았기 때문에 가능한 것.
+        /*HashSet<List<Integer>> set = new HashSet<>();
+        set.add(List.of(1,2,3));
+        set.add(List.of(1,2,3));
+        System.out.println(set);*/
+
+        System.out.println(canArrange(new int[]{1, 2, 3, 4, 5, 6, 15, -1, -2, 5}, 10));
+        System.out.println(canArrange(new int[]{75,5,-5,75,-2,-3,88,10,10,87}, 85));
+    }
+
+    //solutions를 보니까 나 혼자 계속 고민해서는 절대 못 풀 문제였음.... https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/solutions/5854226/can-you-pair-these-numbers-beats-100-explained-with-video-c-java-python-js-explained/?envType=daily-question&envId=2024-10-01
+    public static boolean canArrange(int[] arr, int k) {
+        int [] freq = new int[k];
+        for(int num: arr){
+            int rem = num % k;
+            if(rem < 0){
+                rem = rem + k;
+            }
+            freq[rem]++;
+        }
+        if(freq[0] % 2 != 0){
+            return false;
+        }
+        for(int i = 1;i <= k / 2;i++){
+            if(freq[i] != freq[k-i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //코드를 이렇게 고치니까 이제 정상적으로 동작하기는 하지만, 입력의 크기가 매우 클 때 시간초과가 발생한다.
+    /*public static boolean canArrange(int[] arr, int k) {
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i < arr.length-1; i++) {//투포인터 탐색 범위를 점차 줄여감
+            for (int j = i+1; j < arr.length; j++) {//움직이는 포인터
+                if ((arr[i] + arr[j]) % k == 0) {
+                    if (!(set.contains(i) || set.contains(j))) {
+                        set.add(i);
+                        set.add(j);
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            if (count == arr.length / 2) {
+                return true;
+            }
+        }
+
+        System.out.println(count);
+
+        return false;
+    }*/
+
+    /*public static boolean canArrange(int[] arr, int k) {
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        HashSet<List<Integer>> listSet = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length-1; i++) {//투포인터 탐색 범위를 점차 줄여감
+            for (int j = i+1; j < arr.length; j++) {//움직이는 포인터
+                if ((arr[i] + arr[j]) % k == 0) {
+                    *//*if (!(set.contains(i) || set.contains(j))) {
+                        set.add(i);
+                        set.add(j);
+                        listSet.add(List.of(arr[i], arr[j]));//조합 중복을 무지성으로 제거해서는 안됨 [5,5,10,10] k=15 이런 경우를 염두에 둬야 한다.
+                        break;
+                    }*//*
+                    listSet.add(List.of(arr[i], arr[j]));
+                }
+            }
+        }
+
+        System.out.println(listSet);
+
+        return listSet.size() >= arr.length / 2;
+    }*/
+}
