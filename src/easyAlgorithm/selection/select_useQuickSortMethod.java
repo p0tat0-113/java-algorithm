cbb5338a8d551aadd7456b3f840ca549bb7dee96
gamja3 <dwkimdwkim1005@gmail.com>
2024-10-04 01:07:41 +0900

    둘 다 책에 나온 quickSelect 구조를 그대로 사용하게끔 수정함. 더 세련되고 이해하기도 쉽네.

--- src/easyAlgorithm/selection/select_useQuickSortMethod.java
+++ src/easyAlgorithm/selection/select_useQuickSortMethod.java
@@ -5,13 +5,35 @@ import java.util.Random;
 public class select_useQuickSortMethod {
     public static void main(String[] args) {
         int[] arr = {5,2,3,4,1};
-        int i = 5;
+        int i = 4;
 
         Random random = new Random();
-        System.out.println(select(arr, 0, arr.length-1, i-1, random));//arr.length-1로 하니까 가장 큰 숫자를 고르는 경우가 안됨.
+        System.out.println(select(arr, 0, arr.length-1, i, random));//arr.length-1로 하니까 가장 큰 숫자를 고르는 경우가 안됨.
     }
 
-    private static int select(int[] arr, int s, int e, int i, Random random){//i번째로 작은 수의 인덱스를 반환함. 없으면 -1을 반환한다.
+    //책에 나온 그대로 좀 더 세련되게 만들어봤다.
+    private static int select(int[] arr, int p, int r, int i, Random random){
+        if (p == r) {
+            return arr[p];
+        }
+        //pivot을 랜덤으로 선정
+        int randomIdx = random.nextInt(p,r+1);
+        int temp = arr[randomIdx];
+        arr[randomIdx] = arr[p];
+        arr[p] = temp;
+
+        int q = partition(arr,p,r);
+        int k = q-p+1;//k는 pivot이 부분배열 내에서 몇 번째로 작은 수인지를 나타낸다. k는 부분배열 내에서 pivot이 상대적으로 몇번째 작은 수인지를 나타내는 것 뿐이므로, 재귀호출시 인덱스 범위는 당연히 q를 사용해서 나타내야 한다.
+        if (i < k) {//이 경우 왼쪽 그룹을 뒤져봐야 함.
+            return select(arr,p,q-1, i, random);//찾고자 하는 수는 왼쪽 그룹 안에서 여전히 i번째로 작은 수다.
+        } else if (i == k) {
+            return arr[q];
+        } else {//이 경우 오른족 그룹을 뒤져봐야 함.
+            return select(arr,q+1, r, i-k, random);//찾고자 하는 수는 오른쪽 그룹 안에서 i-k번째로 작은 수다.
+        }
+    }
+
+    /*private static int select(int[] arr, int s, int e, int i, Random random){//i번째로 작은 수의 인덱스를 반환함. 없으면 -1을 반환한다.
         if (s < e) {
             //pivot을 랜덤으로 뽑아서 partition의 성능을 향상시킨다.
             int randomIdx = random.nextInt(s,e+1);
@@ -24,25 +46,20 @@ public class select_useQuickSortMethod {
                 return select(arr,s,pivot-1, i, random);
             } else if (i == pivot) {//i == pivot를 먼저 검사하게 하니까 되네.
                 return arr[pivot];
-            } else {//i == pivot인 경우, i번째로 작은 수를 찾았음.
+            } else {
                 return select(arr,pivot+1, e, i, random);
             }
         }
         return -1;
-    }
+    }*/
 
     private static int partition(int[] arr, int s, int e){//기준원소를 알맞은 위치로 옮기고, 그 인덱스를 반환한다.
         int pivot = arr[s];//첫번째 원소를 pivot으로 선택한다.
         //이번에는 책에 나온 1구역, 2구역, 3구역 방식을 사용해보자.
         int first = s;
         int second = s+1;
-        while(second <= e) {//second <= e로 하니까 가장 큰 숫자를 고르는 경우가 안됨.
-            if (pivot > arr[second]){
-                first++;
-                int temp = arr[second];
-                arr[second] = arr[first];
-                arr[first] = temp;
-            } else if (pivot == arr[second] && second%2 == 0) {//중복된 요소가 많은 경우에 균등하게 분배되게 해서 성능을 개선한다.
+        while(second <= e) {
+            if (pivot > arr[second] || (pivot == arr[second] && second%2 == 0)){//pivot과 같은 값이 있을 때 양쪽으로 균등하게 분배하는 부분의 코드도 중복을 제거함.
                 first++;
                 int temp = arr[second];
                 arr[second] = arr[first];
--- src/leetcode/medium/Leet_215KthLargestElementInArray_1.java
+++ src/leetcode/medium/Leet_215KthLargestElementInArray_1.java
@@ -21,37 +21,37 @@ public class Leet_215KthLargestElementInArray_1 {
         //int[] nums = {2,1};
         //int[] nums = {3,2,3,1,2,4,5,5,6};
 
-        System.out.println(findKthLargest(nums, 2));
+        System.out.println(findKthLargest(nums, 4));
 
         /*Random random = new Random();
         System.out.println(random.nextInt(1, 3));*/
     }
 
     public static int findKthLargest(int[] nums, int k) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
         Random random = new Random();
-        return select(nums, 0, nums.length, nums.length-k, random);//배열의 길이가 6일 때, 2번째로 큰 수는 5번째로 작은 수와 같다.
+        return select(nums, 0, nums.length - 1, nums.length - k + 1, random);//배열의 길이가 6일 때, 2번째로 큰 수는 5번째로 작은 수와 같다.
     }
 
-    private static int select(int[] arr, int s, int e, int i, Random random){//i번째로 작은 수의 인덱스를 반환함. 없으면 -1을 반환한다.
-        if (s < e) {
-            int randomIdx = random.nextInt(s,e);
-            int temp = arr[randomIdx];
-            arr[randomIdx] = arr[s];
-            arr[s] = temp;
-
-            int pivot = partition(arr,s,e);
-            if (i < pivot) {
-                return select(arr,s,pivot, i, random);
-            } else if (i > pivot) {
-                return select(arr,pivot+1, e, i, random);
-            } else {//i == pivot인 경우, i번째로 작은 수를 찾았음.
-                return arr[pivot];
-            }
+    private static int select(int[] arr, int p, int r, int i, Random random){//i번째로 작은 수의 인덱스를 반환함. 없으면 -1을 반환한다.
+        if (p == r) {
+            return arr[p];
         }
-        return -1;
+
+        int randomIdx = random.nextInt(p, r+1);
+        int temp = arr[randomIdx];
+        arr[randomIdx] = arr[p];
+        arr[p] = temp;
+
+        int q = partition(arr, p, r);
+        int k = q - p + 1;
+
+        if (i < k) {
+            return select(arr,p,q-1,i,random);
+        } else if (i == k) {
+            return arr[q];
+        } else {
+            return select(arr,q+1,r,i-k,random);
+        }
     }
 
     private static int partition(int[] arr, int s, int e){//기준원소를 알맞은 위치로 옮기고, 그 인덱스를 반환한다.
@@ -59,13 +59,8 @@ public class Leet_215KthLargestElementInArray_1 {
         //이번에는 책에 나온 1구역, 2구역, 3구역 방식을 사용해보자.
         int first = s;
         int second = s+1;
-        while(second < e) {//second <= e로 하니까 가장 큰 숫자를 고르는 경우가 안됨.
-            if (pivot > arr[second]){
-                first++;
-                int temp = arr[second];
-                arr[second] = arr[first];
-                arr[first] = temp;
-            } else if (pivot == arr[second] && second%2 == 0) {
+        while(second <= e) {//지금의 e는 부분배열의 맨 끝 인덱스를 가리키므로 <=로 해야 한다.
+            if (pivot > arr[second] || (pivot == arr[second] && second % 2 == 0)){
                 first++;
                 int temp = arr[second];
                 arr[second] = arr[first];
