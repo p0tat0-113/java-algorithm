d0059bb51f8c6bab01893e97991dcb0931d4e776
gamja3 <dwkimdwkim1005@gmail.com>
2024-10-03 15:55:03 +0900

    배열에서 I번째 작은 수를 선형시간에 찾아내는 알고리즘, 퀵 정렬의 응용이다.

---
+++ src/easyAlgorithm/selection/select_useQuickSortMethod.java
@@ -0,0 +1,45 @@
+package easyAlgorithm.selection;
+
+public class select_useQuickSortMethod {
+    public static void main(String[] args) {
+        int[] arr = {5,2,3,4,1};
+        int i = 5;
+
+        System.out.println(select(arr, 0, arr.length, i-1));
+    }
+
+    private static int select(int[] arr, int s, int e, int i){//i번째로 작은 수의 인덱스를 반환함. 없으면 -1을 반환한다.
+        if (s < e) {
+            int pivot = partition(arr,s,e);
+            if (i < pivot) {
+                return select(arr,s,pivot-1, i);
+            } else if (i > pivot) {
+                return select(arr,pivot+1, e, i);
+            } else {//i == pivot인 경우, i번째로 작은 수를 찾았음.
+                return arr[pivot];
+            }
+        }
+        return -1;
+    }
+
+    private static int partition(int[] arr, int s, int e){//기준원소를 알맞은 위치로 옮기고, 그 인덱스를 반환한다.
+        int pivot = arr[s];//첫번째 원소를 pivot으로 선택한다.
+        //이번에는 책에 나온 1구역, 2구역, 3구역 방식을 사용해보자.
+        int first = s;
+        int second = s+1;
+        while(second < e) {
+            if (pivot > arr[second]){
+                first++;
+                int temp = arr[second];
+                arr[second] = arr[first];
+                arr[first] = temp;
+            }
+            second++;
+        }
+
+        arr[s] = arr[first];
+        arr[first] = pivot;
+
+        return first;
+    }
+}
