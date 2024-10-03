a5f2b8224c31be4fcbbbae2f971964f313b5d1d2
gamja3 <dwkimdwkim1005@gmail.com>
2024-10-03 17:18:35 +0900

    배열에서 I번째 작은 수를 선형시간에 찾아내는 알고리즘, 퀵 정렬의 응용이다.

--- src/easyAlgorithm/selection/select_useQuickSortMethod.java
+++ src/easyAlgorithm/selection/select_useQuickSortMethod.java
@@ -5,7 +5,7 @@ public class select_useQuickSortMethod {
         int[] arr = {5,2,3,4,1};
         int i = 5;
 
-        System.out.println(select(arr, 0, arr.length, i-1));
+        System.out.println(select(arr, 0, arr.length-1, i-1));//arr.length-1로 하니까 가장 큰 숫자를 고르는 경우가 안됨.
     }
 
     private static int select(int[] arr, int s, int e, int i){//i번째로 작은 수의 인덱스를 반환함. 없으면 -1을 반환한다.
@@ -13,10 +13,10 @@ public class select_useQuickSortMethod {
             int pivot = partition(arr,s,e);
             if (i < pivot) {
                 return select(arr,s,pivot-1, i);
-            } else if (i > pivot) {
-                return select(arr,pivot+1, e, i);
-            } else {//i == pivot인 경우, i번째로 작은 수를 찾았음.
+            } else if (i == pivot) {//아 i == pivot를 먼저 검사하게 하니까 되네.
                 return arr[pivot];
+            } else {//i == pivot인 경우, i번째로 작은 수를 찾았음.
+                return select(arr,pivot+1, e, i);
             }
         }
         return -1;
@@ -27,7 +27,7 @@ public class select_useQuickSortMethod {
         //이번에는 책에 나온 1구역, 2구역, 3구역 방식을 사용해보자.
         int first = s;
         int second = s+1;
-        while(second < e) {
+        while(second <= e) {//second <= e로 하니까 가장 큰 숫자를 고르는 경우가 안됨.
             if (pivot > arr[second]){
                 first++;
                 int temp = arr[second];
