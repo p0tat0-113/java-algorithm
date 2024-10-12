5987c6089bd80d4df4d0e5e16ea906df97f120e1
gamja3 <dwkimdwkim1005@gmail.com>
2024-10-13 01:23:04 +0900

    리트코드 105번 문제, 성능 최적화 완료!

--- src/leetcode/medium/Leet_105ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/leetcode/medium/Leet_105ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,6 +1,14 @@
 package leetcode.medium;
 
 import leetcode.TreeNode;
+import java.util.HashMap;
+
+/*
+https://imgur.com/a/NgiUUHg
+전위순회와 중위순회 탐색의 결과 배열을 서로 보완시켜서 원래의 트리를 만들어내야 하는 문제다.
+어떻게 할까 고민하다가 분할정복 방식으로 하니까 풀렸음.
+좀 더 고민하다가 map을 도입하니까 속도도 빨라졌음.
+*/
 
 public class Leet_105ConstructBinaryTreeFromPreorderAndInorderTraversal {
     public static void main(String[] args) {
@@ -9,25 +17,31 @@ public class Leet_105ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
     int[] preorder;
     int[] inorder;
+    HashMap<Integer,Integer> map;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         this.preorder = preorder;
         this.inorder = inorder;
+        this.map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i],i);
+        }
+
         return process(0, 0, inorder.length-1);
     }
 
     private TreeNode process(int rootIdx, int s, int e){
-        if (rootIdx < preorder.length && s <= e) {
+        if (s <= e) {
             TreeNode root = new TreeNode(preorder[rootIdx]);
 
             //inorder에서 root의 위치를 확인한다.
-            int rootIdxInInorder = 0;
-            for (int i = s; i <= e; i++) {
+            int rootIdxInInorder = map.get(rootIdx);//매번 배열을 순회하면서 찾지 않고, 그냥 처음에 HashMap을 하나 만들어서 접근을 시키니까 1ms까지 성능개선에 성공함. 내 접근방식에는 문제가 없었음.
+            /*for (int i = s; i <= e; i++) {
                 if (preorder[rootIdx] == inorder[i]) {
                     rootIdxInInorder = i;
                     break;
                 }
-            }
+            }*/
 
             int k = rootIdxInInorder-s+1;//root의 inorder부분 배열 내에서의 등수
 
@@ -36,11 +50,9 @@ public class Leet_105ConstructBinaryTreeFromPreorderAndInorderTraversal {
             }
             root.right = process(rootIdx + k, rootIdxInInorder+1, e);
 
-
             return root;
-        } else {
-            return null;
         }
+        return null;
     }
 
     //처음의 코드
