b3ec719d486e536b47a80638ee9c152ac3288d5c
gamja3 <dwkimdwkim1005@gmail.com>
2024-11-19 21:00:39 +0900

    리트코드 79번 문제 해결완료. 정말 간단한 코드 한 줄을 빼먹어서 문제가 발생하고 있던 거였다. 항상 dfs의 본질에 대해서 생각하면서 문제를 풀자.

--- src/leetcode/medium/Leet_79WordSearch.java
+++ src/leetcode/medium/Leet_79WordSearch.java
@@ -17,9 +17,11 @@ public class Leet_79WordSearch {
     }
 
     public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
         for (int row = 0; row < board.length; row++) {
             for (int column = 0; column < board[0].length; column++) {
-                if (dfs(row, column,0, word, board, new boolean[board.length][board[0].length])) {
+                if (dfs(row, column,0, word, board, visited)) {
                     return true;
                 }
             }
@@ -33,22 +35,21 @@ public class Leet_79WordSearch {
             return true;
         }
 
+        //row, column값이 범위를 벗어나거나, 앞서 이미 방문한 곳이거나, 깊이와 문자가 일치하지 않는 경우 false를 반환한다.
         if ((row < 0 || row >= board.length)
                 || (column < 0 || column >= board[0].length)
-                || visited[row][column]) {
+                || visited[row][column]
+                || board[row][column] != word.charAt(depth)) {
             return false;
         }
-
         visited[row][column] = true;
-
-        if (board[row][column] != word.charAt(depth)) {
-            return false;
-        }
 
         boolean goUP = dfs(row + 1, column, depth + 1, word, board, visited);
         boolean goDOWN = dfs(row - 1, column, depth + 1, word, board, visited);
         boolean goLEFT = dfs(row, column + 1, depth + 1, word, board, visited);
         boolean goRIGHT = dfs(row, column - 1, depth + 1, word, board, visited);
+
+        visited[row][column] = false;//계속 테스트케이스가 하나씩 걸려서 뭐가 문제인가 했는데 이 부분이 없어서 그랬다. dfs면 재귀호출이 끝나고 돌와왔을 때 visited를 다시 해제해주는게 당연한건데 이걸 빼먹었네.
 
         return (goUP || goDOWN || goLEFT || goRIGHT);
     }
