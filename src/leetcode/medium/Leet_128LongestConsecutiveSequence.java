f8a8c65a3cb05bf8c742a8f675845cf227d9b9ab
gamja3 <dwkimdwkim1005@gmail.com>
2024-11-29 00:52:34 +0900

    리트코드 128번 해결완료. O(n)으로 풀기는 했는데, 해시연산을 하는 오버헤드가 커서 그런 것인지 속도가 그렇게 빠르지는 않다. 생각보다 애를 좀 먹었음.

--- src/leetcode/medium/Leet_128LongestConsecutiveSequence.java
+++ src/leetcode/medium/Leet_128LongestConsecutiveSequence.java
@@ -23,48 +23,54 @@ public class Leet_128LongestConsecutiveSequence {
     }
 
     public int longestConsecutive(int[] nums) {
-        HashMap<Integer, Integer> indexMap = new HashMap<>();
-        HashMap<Integer, Integer> lengthOfSequenceStartedFrom = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            indexMap.put(nums[i], i);
+        if(nums.length == 0) {
+            return 0;
         }
 
-        System.out.println("초기화");
-        System.out.println(indexMap);
+        HashMap<Integer, Integer> used = new HashMap<>();//어떤 숫자가 nums에 존재하는지, 그리고 이 숫자가 수열을 만드는게 사용되었는지 확인하는 기능을 둘 다 수행한다.
+        HashMap<Integer, Integer> lengthOfSequenceStartedFrom = new HashMap<>();
+
+        for (int num : nums) {
+            used.put(num, 0);//0은 사용되지 않았음을 의미한다.
+        }
+
+        /*System.out.println("초기화");
         System.out.println(Arrays.toString(nums));
+        System.out.println(used);
         System.out.println(lengthOfSequenceStartedFrom);
 
-        System.out.println("순회시작");
+        System.out.println("순회시작");*/
         for (int i = 0; i < nums.length; i++) {
             int headNum = nums[i];
 
-            System.out.println(headNum);
+            /*System.out.println(headNum);
+            System.out.println(used);*/
 
-            if (headNum == Integer.MIN_VALUE) {//이미 사용된 숫자라면 패스함.
-                System.out.println("continue");
+            if (used.get(headNum) == 1) {//이미 사용된 숫자라면 패스함.
+                /*System.out.println("continue");*/
                 continue;
             }
 
-            System.out.println(lengthOfSequenceStartedFrom);
+            /*System.out.println(lengthOfSequenceStartedFrom);*/
 
-            lengthOfSequenceStartedFrom.put(nums[i],1);//lengthOfSequenceStartedFrom에 nums[i]로 시작하는 수열의 길이가 1임을 입력
+            lengthOfSequenceStartedFrom.put(nums[i],1);//lengthOfSequenceStartedFrom에 headNum으로 시작하는 수열의 길이가 1임을 입력
             int nextNum = headNum + 1;
-            nums[i] = Integer.MIN_VALUE;//headNum이 사용되었음을 표시
+            used.put(headNum, 1);//headNum이 사용되었음을 표시
 
 
             while (true) {
-                if (!indexMap.containsKey(nextNum)) {//nextNum이 nums에 존재하지 않는 숫자라면 바로 탈출한다.
+                if (!used.containsKey(nextNum)) {//nextNum이 nums에 존재하지 않는 숫자라면 바로 탈출한다.
                     break;
                 }
-                if (nums[indexMap.get(nextNum)] == Integer.MIN_VALUE) {//nextNum이 사용된 숫자라면 이 숫자를 시작하는 수열이 반드시 있을 것이다. 이 수열의 길이를 가져와서 더하고 탈출한다.
+                if (used.get(nextNum) == 1) {//nextNum이 사용된 숫자라면 이 숫자를 시작하는 수열이 반드시 있을 것이다. 이 수열의 길이를 가져와서 더하고 탈출한다.
                     lengthOfSequenceStartedFrom.put(headNum, lengthOfSequenceStartedFrom.get(headNum) + lengthOfSequenceStartedFrom.get(nextNum));
                     break;
                 }
 
-                nums[indexMap.get(nextNum)] = Integer.MIN_VALUE;//nextNum이 어떤 수열에 사용되었음을 표시
-                lengthOfSequenceStartedFrom.put(headNum, lengthOfSequenceStartedFrom.get(headNum) + 1);
-                nextNum += 1;
+                //nextNum이 nums에 존재하고, 아직 사용되지 않은 경우
+                used.put(nextNum, 1);//nextNum이 사용되었음을 표시
+                lengthOfSequenceStartedFrom.put(headNum, lengthOfSequenceStartedFrom.get(headNum) + 1);//headNum으로 시작하는 수열의 길이 1증가
+                nextNum += 1;//다음 연산을 위해 nextNum 1증가
             }
         }
 
