7b89cee6075bfb75396ff45541a0c58974aec7cc
gamja3 <dwkimdwkim1005@gmail.com>
2024-12-04 23:16:47 +0900

    리트코드 787번 문제 CheapestFlightWithinKStops를 벨만포드 알고리즘을 이용해서 풀어보고 있다. 뭔가 이상하게 작동해서 잘 생각해보니까 {0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0 이 케이스에서 어쩌다 보니 간선 검사가 연쇄적으로 간선을 2개 사용할 때의 최단거리를 구하고 있다. 이걸 해결해야 함.

---
+++ src/leetcode/medium/Leet_787CheapestFlightWithinKStops_BellmanFord.java
@@ -0,0 +1,71 @@
+package leetcode.medium;
+
+/*
+노드의 총 갯수인 n, 간선정보(방향, 가중치) flights배열, 시작정점, 도착정점, 최대 경유 가능 노드 수 k를 입력 받는다.
+중간에 최대 k개의 노드만 경유할 때 시작정점에서 도착정점으로 가는 최소비용을 찾는 문제다.
+
+743번 NetworkDelayTime문제의 Node에 이 distance를 얻기 위해 경유한 노드의 수 값까지 기록하게 하는 식으로 해도 될 것 같기도 하고,
+그냥 dfs로 풀어도 될 것 같기도 하다.
+
+map에 각 노드의 간선정보를 입력하고, dfs로 그걸 타고 이동한다. 만약 타고 가다가 경유한 수가 k를 초과하면 그대로 리턴시켜버리고,
+k를 초과하지 않고 목적지에 도달하면 비용을 기록하는 식으로 하면 될 듯. <- 이 방식으로 접근했고, 잘 작동되기는 하는데 간선의 수가 많은 경우 시간초과가 뜬다.
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Leet_787CheapestFlightWithinKStops_BellmanFord {
+    //이번에는 진짜 벨만포드로 풀어본다. 이 문제는 벨만포드로 푸는게 가장 간단해보임. 우선 벨만포드의 수도코드부터 보자
+    /*
+    BellmanFord(G) {
+        foreach u in V {
+            u.d = infinite;
+        }
+        r.d = 0;
+
+        for i = 1 to |V|-1 {
+            foreach (u,v) in E {
+                if v.d > u.d + Wuv {
+                    v.d = u.d + Wuv
+                }
+            }
+        }
+    }
+    */
+    //벨만포드는 모든 간선을 검사하는 것을 V-1번 반복해서 시작정점에서 모든정점에 이르는 최단거리를 계산한다.
+    //모든 간선을 검사하는 과정을 i번 반복하면 간선을 최대 i개 사용했을 때의 최단거리가 구해진다.
+    //이것을 V-1에서 k로 바꾸기만 하면 된다.
+    //지금 이 문제는 입력으로 간선을 던져주기 때문에 벨만포드로 구현하기에 딱 좋다.
+
+    public static void main(String[] args) {
+        Leet_787CheapestFlightWithinKStops_BellmanFord leet = new Leet_787CheapestFlightWithinKStops_BellmanFord();
+        System.out.println(leet.findCheapestPrice(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 1));
+        System.out.println(leet.findCheapestPrice(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0));
+    }
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] distanceArr = new int[n];
+        Arrays.setAll(distanceArr, e -> Integer.MAX_VALUE);
+        distanceArr[src] = 0;
+
+        for (int i = 0; i <= k; i++) {
+            for (int[] flight : flights) {
+                int dU = distanceArr[flight[0]];
+                int dV = distanceArr[flight[1]];
+                int Wuv = flight[2];
+
+                if (dU == Integer.MAX_VALUE) {
+                    continue;
+                }
+                if (dV > dU + Wuv) {
+                    distanceArr[flight[1]] = dU + Wuv;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(distanceArr));
+        return 0;
+    }
+}
