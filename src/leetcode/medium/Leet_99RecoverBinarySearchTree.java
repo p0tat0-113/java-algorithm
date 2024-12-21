72ea0291e7c522ec2f9cfe461d5c58cc9ba27548
gamja3 <dwkimdwkim1005@gmail.com>
2024-12-22 02:50:02 +0900

    리트코드 99번 RecoverBinarySearchTree문제 해결완료!

--- src/leetcode/medium/Leet_99RecoverBinarySearchTree.java
+++ src/leetcode/medium/Leet_99RecoverBinarySearchTree.java
@@ -1,9 +1,11 @@
 package leetcode.medium;
 
-import com.sun.source.tree.Tree;
 import leetcode.TreeNode;
 
-import java.util.LinkedList;
+/*
+문제를 풀긴 풀었는데 이게 재귀호출이라서 문제에서 follow up으로 제시하는 것처럼 공간복잡도가 O(1)은 아니다.
+https://imgur.com/a/3FfsWyb
+*/
 
 public class Leet_99RecoverBinarySearchTree {
     public static void main(String[] args) {
@@ -14,123 +16,54 @@ public class Leet_99RecoverBinarySearchTree {
         root1.right = new TreeNode(4);
         root1.right.left = new TreeNode(2);
 
-        leet.printPreorder(root1);
-        System.out.println();
-
-        leet.recoverTree(root1);
-
-        leet.printPreorder(root1);
-        System.out.println();
-
-        System.out.println("----------------");
-
         TreeNode root2 = new TreeNode(2);
         root2.left = new TreeNode(3);
         root2.right = new TreeNode(1);
-
-        leet.printPreorder(root2);
-        System.out.println();
-
-        leet.recoverTree(root2);
-
-        leet.printPreorder(root2);
-        System.out.println();
-
-        System.out.println("----------------");
 
         TreeNode root3 = new TreeNode(3);
         root3.right = new TreeNode(2);
         root3.right.right = new TreeNode(1);
 
-        leet.printPreorder(root3);
-        System.out.println();
-
+        leet.recoverTree(root1);
+        leet.recoverTree(root2);
         leet.recoverTree(root3);
-
-        leet.printPreorder(root3);
-        System.out.println();
-
-
     }
+
+    private TreeNode prev = null;
+    private TreeNode first = null;
+    private TreeNode second = null;
 
     public void recoverTree(TreeNode root) {
-        LinkedList<TreeNode> smallerNodesStack = new LinkedList<>();
-        LinkedList<TreeNode> biggerNodesStack = new LinkedList<>();
-        LinkedList<TreeNode[]> violated = new LinkedList<TreeNode[]>();
+        inorder(root);
+        //System.out.println(first.val);
+        //System.out.println(second.val);
+        //System.out.println("---------------------");
 
-        dfs(root, smallerNodesStack,biggerNodesStack, violated);
-
-        if (violated.size() == 1) {
-            int temp = violated.get(0)[0].val;
-            violated.get(0)[0].val = violated.get(0)[1].val;
-            violated.get(0)[1].val = temp;
-        } else {
-            int temp = violated.get(0)[1].val;
-            violated.get(0)[1].val = violated.get(1)[1].val;
-            violated.get(1)[1].val = temp;
-        }
-
-        //System.out.println(violated.size());
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
 
-    //자꾸 테스트 케이스가 하나씩 걸려서 코드를 좀 많이 고쳐야 할 것 같다...
-    //일단 경로를 쭉 탐색하고 있는 동안의 violation포인트가 몇개가 나오던지 무시하고 가장 최신 것만 쭉 밀고 가는게 맞는 것 같다. 그래야 테스트케이스4 같은 경우가 해결됨.
-    //아 근데 이렇게 하면 또 테스트케이스5가 해결이 안됨... 뭔가 다른 돌파구가 필요한데...
-
-    private void dfs(TreeNode root, LinkedList<TreeNode> smallerNodesStack, LinkedList<TreeNode> biggerNodesStack, LinkedList<TreeNode[]> violated){
-        if (root == null) {
+    private void inorder(TreeNode root) {
+        if (root == null)  {
             return;
         }
 
-        if (!smallerNodesStack.isEmpty()) {
-            TreeNode latestSmallerNode = smallerNodesStack.peekLast();
-            if (latestSmallerNode.val > root.val) {//조건위반감지
+        inorder(root.left);
 
-                if (!violated.isEmpty()) {
-                    TreeNode[] treeNodes = violated.peekLast();
-                    if (latestSmallerNode == treeNodes[1]) {
-                        latestSmallerNode = treeNodes[0];
-                        violated.pollLast();
-                    }
+        //여기에서 제약조건 위반을 감지한다. BST 중위순회에서 현재 숫자는 앞의 숫자보다 커야 한다.
+        if (prev != null) {//지금이 첫 시작점이 아닌 경우
+            if (prev.val > root.val) {//제약조건 위반상황이 발생. 현재 숫자가 앞의 숫자보다 작다.
+                if (first == null) {//이전까지 제약조건 위반상황이 없었던 경우.
+                    first = prev;
+                    second = root;
+                } else {//이전에 제약조건 위반상황이 있었던 경우. [3,null,2,null,1], [2,3,1] 이런 테스트케이스에서 요 상황이 발생한다.
+                    second = root;
                 }
-                violated.add(new TreeNode[] {latestSmallerNode,root});
-                System.out.println(violated);
-                //return;
             }
         }
+        prev = root;//prev로 현재 루트를 집어넣고 다음으로 넘어간다.
 
-        if (!biggerNodesStack.isEmpty()) {
-            TreeNode latestBiggerNode = biggerNodesStack.peekLast();
-            if (latestBiggerNode.val < root.val) {//조건위반감지
-                if (!violated.isEmpty()) {
-                    TreeNode[] treeNodes = violated.peekLast();
-                    if (latestBiggerNode == treeNodes[1]) {
-                        latestBiggerNode = treeNodes[0];
-                        violated.pollLast();
-                    }
-                }
-                violated.add(new TreeNode[] {latestBiggerNode,root});
-                System.out.println(violated);
-                //return;
-            }
-        }
-
-        biggerNodesStack.add(root);
-        dfs(root.left, smallerNodesStack, biggerNodesStack, violated);
-        biggerNodesStack.pollLast();
-
-        smallerNodesStack.add(root);
-        dfs(root.right, smallerNodesStack, biggerNodesStack, violated);
-        smallerNodesStack.pollLast();
-    }
-
-    private void printPreorder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        System.out.print(root.val + " ");
-        printPreorder(root.left);
-        printPreorder(root.right);
+        inorder(root.right);
     }
 }
