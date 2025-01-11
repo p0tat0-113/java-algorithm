e1185ad62eac25c5ccf5bbd1459bb74c4461a113
gamja3 <dwkimdwkim1005@gmail.com>
2025-01-11 15:31:03 +0900

    백준 2178번 미로탐색 문제 해결완료! 입력 부분을 개선해서 실행시간을 128ms에서 76ms로 향상시켰다!!

--- src/baekjoon/graphTraversal/Baek_2178미로탐색.java
+++ src/baekjoon/graphTraversal/Baek_2178미로탐색.java
@@ -19,6 +19,8 @@ import java.util.StringTokenizer;
 
 bfs를 사용해서 최단경로를 찾는 문제다. bfs의 특성을 사용하여서 해결해야 한다. Leet_787CheapestFlight 문제를 bfs로 풀던 것을 다시 보면 좋을 듯.
 */
+
+//이번에는 시간이 과도하게 오래 걸리는 문제를 해결해본다. 아마도 입력 부분에서 비효율이 발생하는 것으로 보인다.
 public class Baek_2178미로탐색 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,11 +29,16 @@ public class Baek_2178미로탐색 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        String[][] maze = new String[n][m];
+        boolean[][] maze = new boolean[n][m];
         for (int i = 0; i < n; i++) {
-            maze[i] = br.readLine().split("");
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                if (input.charAt(j) == '1') {
+                    maze[i][j] = true;
+                }
+            }
         }
-        maze[0][0] = "0";
+        maze[0][0] = false;
 
         int distanceCount = 0;
         LinkedList<int[]> queue = new LinkedList<>();
@@ -60,12 +67,12 @@ public class Baek_2178미로탐색 {
     }
 
     //여기에서 규칙위반 여부를 조사해서 큐에 추가한다.
-    private static void addToQueue(String[][] maze, LinkedList<int[]> queue, int row, int column) {
-        if (row < 0 || row >= maze.length || column < 0 || column >= maze[0].length || maze[row][column].equals("0")) {
+    private static void addToQueue(boolean[][] maze, LinkedList<int[]> queue, int row, int column) {
+        if (row < 0 || row >= maze.length || column < 0 || column >= maze[0].length || !maze[row][column]) {
             return;
         }
 
         queue.add(new int[] {row, column});
-        maze[row][column] = "0";//이 부분이 문제였다. 바로바로 방문 표시를 해서 똑같은 좌표가 중복으로 큐에 들어가는 것을 막아주니까 메모리 초과 문제가 해결된다. 전에도 다른 문제를 풀면서 비슷한 문제를 한 번 겪었었다.
+        maze[row][column] = false;//이 부분이 문제였다. 바로바로 방문 표시를 해서 똑같은 좌표가 중복으로 큐에 들어가는 것을 막아주니까 메모리 초과 문제가 해결된다. 전에도 다른 문제를 풀면서 비슷한 문제를 한 번 겪었었다.
     }
 }
